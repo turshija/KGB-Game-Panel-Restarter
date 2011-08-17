f60e93eae92178d204f774fa4667f4635784980a
Boris Vujicic <turshija@gmail.com>
2011-08-17 23:51:25 +0200

    update

--- includes/restarter.class.php
+++ includes/restarter.class.php
@@ -6,24 +6,28 @@ class Restarter {
 		global $config;
 	}
 	
+	/*
+	 * Metoda poziva parsiranje, i vraca listu servera u nizu
+	 */
 	function getServers() {
 		global $config;
 		$servers = array();
 		foreach ($config['tokens'] as $tokenid=>$token) {
 			$feed = "servers.php?auth=".$token;
-			//echo $tokenid." ".$feed."<br />";
-			
 			$temp=$this->parseServersFromFeed($feed,$tokenid);
-			array_push($servers,$temp);
-			//$servers = $servers[0];
-			
+			foreach ($temp as $server) {
+				array_push($servers,$server);	
+			}
 		}
-		
-		print_r($servers);
+		return $servers;
 	}
 	
+	/*
+	 * Metoda cita Gpanel feed, parsira i vraca niz servera sa informacijama
+	 */
 	function parseServersFromFeed($feed,$tokenid) {
 		$br_info = 7;
+		$i=0;
 		$dump = $this->procitaj($feed);
 		$info = explode("\n",$dump);
 		
@@ -37,30 +41,12 @@ class Restarter {
 			$server[$i]['game']			=	$info[(($j-1)*$br_info)+2+2];
 			$server[$i]['ip']			=	$info[(($j-1)*$br_info)+2+3];
 			$server[$i]['slots']		=	$info[(($j-1)*$br_info)+2+4];
-			//$server[$i]['status']		=	$info[(($j-1)*$br_info)+2+5];
 			$server[$i]['online']		=	$info[(($j-1)*$br_info)+2+6];
-			$server[$i]['auth'] = $tokenid;
+			$server[$i]['auth']			=	$tokenid;
 			
-			/*
-			$gtlink = "ip=".$server[$i]['ip'];
-			$gtresp = unserialize($restarter->procitaj($gtlink,"gt"));
-			if ($gtresp['b']['ip']=="") $server[$i]['status']="Nema servera na GT-u";
-			else {
-				if ($server[$i]['online']=="Started")
-					if ($gtresp['b']['status']=="1") {
-						$server[$i]['status']="online";
-						$server[$i]['players']=$gtresp['s']['players']."/".$gtresp['s']['playersmax'];
-					} else $server[$i]['status']="offline";
-				else $server[$i]['status']="Server je stopiran!";
-			}
-			 * 
-			 */
 			$i++;
 		}
-		
 		return $server;
-		
-		
 	}
 	
 	function procitaj($link,$vrsta="panel") {
--- index.php
+++ index.php
@@ -3,4 +3,8 @@ include "main.php";
 
 $servers = $restarter->getServers();
 
+foreach ($servers as $server) {
+	echo $server['ip']." ".$server['name']."<br />";
+}
+
 ?>
