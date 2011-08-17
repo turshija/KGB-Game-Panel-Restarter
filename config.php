1f2155659b66530bd76e79806227fce25ec3fe2c
Boris Vujicic <turshija@gmail.com>
2011-08-18 01:29:19 +0200

    dodat config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+$config['tokens'] = array("ovde ide token");
+
+$config['feedVrsta'] = 1;
+
+$config['apilink'] = "http://www.kgb-hosting.com/gpanel/api/"; 
+$config['gtapilink'] = "http://www.gametracker.rs/feed.php";
+?>
