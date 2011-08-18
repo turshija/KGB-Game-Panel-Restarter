84e2ee95aa85daba5e174f3621ceb462fcb0bd8c
Boris Vujicic <turshija@gmail.com>
2011-08-18 02:15:23 +0200

    Dodata nova opcija

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 $config['tokens'] = array("82fe32a2a0113936343d5d5316626dcd22306dfd0019d4be8a66ae1574194002");
 
 $config['feedVrsta'] = 1;
+$config['broj_skeniranja'] = 3;
 
 $config['apilink'] = "http://www.kgb-hosting.com/gpanel/api/"; 
 $config['gtapilink'] = "http://www.gametracker.rs/feed.php";
