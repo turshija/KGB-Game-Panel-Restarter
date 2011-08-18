42377996c108665230b54e967c01b2b26d944de5
Boris Vujicic <turshija@gmail.com>
2011-08-18 02:15:52 +0200

    Deo koda prebacen uklasu kao zasebna metoda

--- process.php
+++ process.php
@@ -1,19 +1,20 @@
 <?php
 include "main.php";
+$br_skeniranja = $config['broj_skeniranja'];
 
 if ($_POST['task']=="gtstatus") {
 	$ip = $restarter->ocistiIP($_POST['ip']);
 	
-	$link = "ip={$ip}";
-	$server = unserialize($restarter->procitaj($link,"gt"));
+	$server = $restarter->skenirajServer($ip);
 	
-	if (!$server['b']['status']) {
+	if (!$server) {
 		$return['status'] = "No server";
 		$return['players'] = "-";
 		$return['action'] = "hide";
 	} else {
 		$return['status'] = $server['b']['status'];
-		$return['players'] = $server['s']['players']."/".$server['s']['playersmax'];
+		if (!$return['status']) $return['players'] = "-";
+		else $return['players'] = $server['s']['players']."/".$server['s']['playersmax'];
 		$return['action'] = "display";
 	}
 	die(json_encode($return));
