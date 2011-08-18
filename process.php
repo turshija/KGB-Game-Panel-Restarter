b54cf46ada36eaf4aa413f1007d095e1674c7ec5
Boris Vujicic <turshija@gmail.com>
2011-08-18 02:47:16 +0200

    dodat kod za restartovanje servera

--- process.php
+++ process.php
@@ -4,7 +4,6 @@ $br_skeniranja = $config['broj_skeniranja'];
 
 if ($_POST['task']=="gtstatus") {
 	$ip = $restarter->ocistiIP($_POST['ip']);
-	
 	$server = $restarter->skenirajServer($ip);
 	
 	if (!$server) {
@@ -17,6 +16,28 @@ if ($_POST['task']=="gtstatus") {
 		else $return['players'] = $server['s']['players']."/".$server['s']['playersmax'];
 		$return['action'] = "display";
 	}
+	die(json_encode($return));
+	
+} else if ($_POST['task']=="restartServer") {
+	$serverid = (int)$_POST['serverid'];
+	$ip = $restarter->ocistiIP($_POST['ip']);
+	$temp = (int)$_POST['auth'];
+	$token = $config['tokens'][$temp];
+	$server = $restarter->skenirajServer($ip);
+	
+	if (!$server) {
+		$return['msg'] = "No server";
+	} else {
+		if ($server['b']['status']) {
+			$return['msg'] = "Server online";
+		} else {
+			//$return['msg'] = "Restartujem!";
+			$return['msg'] = $restarter->restartujServer($serverid, $token);
+		}
+	}
+	
+	//$return['msg'] = "$serverid $token $ip";
+	
 	die(json_encode($return));
 }
 ?>
