36b1c26427551f8e60fa32b6dc1c5691f0a1b59f
Boris Vujicic <turshija@gmail.com>
2011-08-18 03:31:20 +0200

    Pobrisan visak gluposti

--- index.php
+++ index.php
@@ -8,39 +8,15 @@ foreach ($servers as $server) {
 	echo $server['ip']." ".$server['name']."<br />";
 }*/
 
-?>
-<html>
+?><html>
 <head>
 <title>Restarter</title>
 <script src="assets/js/jquery.min.js" type="text/javascript"></script> 
 <script src="assets/js/main.js" type="text/javascript"></script> 
 <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
-
-<script type="text/javascript"> 
-<!--
-$(document).ready(function(){
-	$("#notif").delay(3000).animate({
-		opacity: 'hide',
-		height: 'toggle'
-	}, 1000, function() {
-		// kraj animacije
-	});
-	
-});
--->
-</script> 
-
 </head>
 
 <body>
-	
-	
-	<?php if (!empty($notifikacija)) { ?>
-	<div id="notif">
-	<?=$notifikacija?>
-	</div>
-	<?php } ?>
-
 	<table align="center">
 		<tr>
 			<th>Ime servera</th>
@@ -66,16 +42,3 @@ OUT;
 	<p id="removedServersNotif" class="labelText">Uklonjeno <span></span> servera sa liste koji nisu dodati na GT.rs!</p>
 </body>
 </html>
-
-
-<div id="mrak" style="width:100%;height:100%;display:none;margin:0px;background-color:#111122;border:0px solid black;filter:alpha(opacity=60);opacity:0.6;position:fixed;top:0px;left:0px;"></div> 
- 
- 
-<div id="load_popup" class="prozor" > 
-<center> 
-	Restartovanje u toku ...<br />
-	<img src="assets/images/status-loader.gif" />
-	
-	
-</center> 
-</div> 
