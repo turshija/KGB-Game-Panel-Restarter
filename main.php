49fdc9e7231bf8058e1ef11f04d3af3ec8a29d7e
Boris Vujicic <turshija@gmail.com>
2012-08-20 13:47:09 +0200

    Added error when there is no config.php

--- main.php
+++ main.php
@@ -1,5 +1,11 @@
 <?php
 ob_start();
-include "config.php";
+if (!@include "config.php") {
+	ob_clean();
+	die('<div style="margin:auto;padding:0px 30px 30px 30px;width:800px;border:2px solid blue;">
+		<h3>Greska</h3>
+		Morate renamovati config.php.default u config.php i upisati unutra svoj token izmedju navodnika !
+		</div>');
+}
 include "assets/classes/restarter.class.php";
 ?>
